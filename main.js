2fc79420a402099e3e7aff8424ecdb54c58f4d8e
Mihael Evđenić <123997671+mevdenic@users.noreply.github.com>
2023-05-04 19:10:22 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -7,33 +7,43 @@ function calculateBMI(){
     let result = weight / Math.pow(height/100, 2);
 
     let total = result.toFixed(2);
+    if(height == "" || weight == ""){
+        alert('Please fill out all forms.');
 
-    if (total < 18.5){
-        document.getElementById("result").innerHTML = 'Your Body Mass Index (BMI) is ' +total+'.<br>You are considered underweight';
+    }else{
+        if (total < 18.5){
+            document.getElementById("result").innerHTML = 'Your Body Mass Index (BMI) is ' +total+'.<br>You are considered underweight';
+        }
+        else if(total > 18.5 && total < 25){
+            document.getElementById("result").innerHTML = 'Your Body Mass Index (BMI) is ' +total+'.<br>Your weight is considered normal.';
+        }
+        else if(total > 25 && total <30){
+            document.getElementById("result").innerHTML = 'Your Body Mass Index (BMI) is ' +total+'.<br>You are considered overweight.';
+        }
+        else if(total > 30){
+            document.getElementById("result").innerHTML = 'Your Body Mass Index (BMI) is ' +total+'.<br>Your are considered obese.';
+        }
     }
-    else if(total > 18.5 && total < 25){
-        document.getElementById("result").innerHTML = 'Your Body Mass Index (BMI) is ' +total+'.<br>Your weight is considered normal.';
-    }
-    else if(total > 25 && total <30){
-        document.getElementById("result").innerHTML = 'Your Body Mass Index (BMI) is ' +total+'.<br>You are considered overweight.';
-    }
-    else if(total > 30){
-        document.getElementById("result").innerHTML = 'Your Body Mass Index (BMI) is ' +total+'.<br>Your are considered obese.';
-    }
+    
 
 }
 
 function calculateRM(){
-
+    
     let weight = document.getElementById('weight').value;
     let reps = document.getElementById('reps').value;
 
     let result = weight / (1.0278 - 0.0278 * parseInt(reps));
 
     let total = result.toFixed(0);
-    if(total > 0 && total < 500){
-        document.getElementById("result").innerHTML = 'Your 1 rep max is approximately '+total+' KG.';
+    if(weight == "" || reps == ""){
+        alert('Please fill out all forms.');
+    }else{
+        if(total > 0 && total < 500){
+            document.getElementById("result").innerHTML = 'Your 1 rep max is approximately '+total+' KG.';
+        }
     }
+    
     
 }
 
@@ -48,32 +58,38 @@ function calculateCal(){
     let result;
     let total;
 
-    if(gender == 1){
-        bmr = 10 * weight + 6.25 * height - 5 * age + 5;
-    }
-    else if(gender == 2){
-        bmr = 10 * weight + 6.25 * height - 5 * age - 161;
+    if(weight == "" || height == "" || age == "" || gender == "" || activity == ""){
+        alert('Please fill out all forms.');
+    }else{
+        if(gender == 1){
+            bmr = 10 * weight + 6.25 * height - 5 * age + 5;
+        }
+        else if(gender == 2){
+            bmr = 10 * weight + 6.25 * height - 5 * age - 161;
+        }
+    
+        if(activity == 1){
+            result = bmr * 1.2;
+            total = result.toFixed(0);
+            document.getElementById("result").innerHTML = 'Maintenance calories: '+total;
+        }
+        else if(activity == 2){
+            result = bmr * 1.375;
+            total = result.toFixed(0);
+            document.getElementById("result").innerHTML = 'Maintenance calories: '+total;
+        }
+        else if(activity == 3){
+            result = bmr * 1.55;
+            total = result.toFixed(0);
+            document.getElementById("result").innerHTML = 'Maintenance calories: '+total;
+        }
+        else if(activity == 4){
+            result = bmr * 1.725;
+            total = result.toFixed(0);
+            document.getElementById("result").innerHTML = 'Maintenance calories: '+total;
+        }
     }
 
-    if(activity == 1){
-        result = bmr * 1.2;
-        total = result.toFixed(0);
-        document.getElementById("result").innerHTML = 'Maintenance calories: '+total;
-    }
-    else if(activity == 2){
-        result = bmr * 1.375;
-        total = result.toFixed(0);
-        document.getElementById("result").innerHTML = 'Maintenance calories: '+total;
-    }
-    else if(activity == 3){
-        result = bmr * 1.55;
-        total = result.toFixed(0);
-        document.getElementById("result").innerHTML = 'Maintenance calories: '+total;
-    }
-    else if(activity == 4){
-        result = bmr * 1.725;
-        total = result.toFixed(0);
-        document.getElementById("result").innerHTML = 'Maintenance calories: '+total;
-    }
+    
     
 }
